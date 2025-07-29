988cd7de6a8861074fb42440d542cc52b80a7a3e
Guneet <guberoi@icloud.com>
2025-07-28 22:17:31 -0400

    added compiler flags

--- lesson0/lesson0-10.cpp
+++ lesson0/lesson0-10.cpp
@@ -20,4 +20,21 @@ in the tasks.json that generate,
 
 the "-g" is for the debug build. change it to (O, not 0): "O1", "O2", or "O3"
 for the optimization level. Lvl 2 is the best choice. 
+
+"-pedantic-errors" adding this removes compiler extensions
+
+do these steps to make sure the compiler doesn't whine.
+Open VS Code and go to Code > Preferences > Settings. This will open a settings dialog.
+Enter "insert final newline" into the search bar.
+In both the Workspace Settings and User Settings tabs, ensure the checkbox labeled Files: Insert Final Newline is checked.
+
+increase errors:
+"-Wall",
+"-Weffc++",
+"-Wextra",
+"-Wconversion",
+"-Wsign-conversion",
+
+treating warning as errors:
+
 */
