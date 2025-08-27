11a9ca305be112a9ec27b0304c39edfed9416c0a
Guneet <guberoi@icloud.com>
2025-08-27 10:31:19 -0400

    done with lesson 7

---
+++ lesson7/declerations.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // this is a decleration
+    using std::cout;        // I didnt import all of std, but I don't need to type it each time for cout
+
+    cout << "Hello.\n";
+
+    return 0;
+}
---
+++ lesson7/directives.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    using namespace std;            // this is a directive since everything was imported in this scope
+
+    cout << "Hello. This is not preffered.\n";
+
+    return 0;
+}
