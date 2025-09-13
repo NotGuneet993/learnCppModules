0c20a54f7826e169572bdfdb1e6675ecfd29148a
Guneet <guberoi@icloud.com>
2025-09-13 19:45:18 -0400

    progress

---
+++ lesson25/vitual.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string_view>
+
+class A
+{
+public:
+    virtual std::string_view getName() const { return "A"; }
+};
+
+class B: public A
+{
+public:
+    virtual std::string_view getName() const { return "B"; }
+};
+
+class C: public B
+{
+public:
+    virtual std::string_view getName() const { return "C"; }
+};
+
+class D: public C
+{
+public:
+    virtual std::string_view getName() const { return "D"; }
+};
+
+int main()
+{
+    C c {};
+    A& rBase{ c }; 
+    std::cout << "rBase is a " << rBase.getName() << '\n';      // this will return C::getName();
+
+    return 0;
+}
+
+
+/*
+    making a class virtual and adding '= 0' to the end makes it abstract. 
+*/
